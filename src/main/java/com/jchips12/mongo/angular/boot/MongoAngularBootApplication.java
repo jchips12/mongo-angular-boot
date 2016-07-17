9634f9b9690fd95d38c7a693ccbb3bab2f78667d
JC Hipolito <jchips12@gmail.com>
2016-07-17 11:06:29 +0800

    initial commit

---
+++ src/main/java/com/jchips12/mongo/angular/boot/MongoAngularBootApplication.java
@@ -0,0 +1,12 @@
+package com.jchips12.mongo.angular.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAngularBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoAngularBootApplication.class, args);
+	}
+}
